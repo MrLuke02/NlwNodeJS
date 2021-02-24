37113c3488c6f4f47f5e78c64a80a6b9e6cb51ba
Lucas <voggavcg601@gmail.com>
2021-02-24 20:11:09 -0300

    Day_3

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import { UserController } from './controllers/userController';
+import { SurveyController } from './controllers/surveyController';
 
 const router = Router();
 const userController = new UserController();
+const surveyController = new SurveyController();
+
+
+
 router.post("/users", userController.create);
+router.post("/surveys", surveyController.create);
+
+router.get("/surveys", surveyController.show);
 
 export { router };
